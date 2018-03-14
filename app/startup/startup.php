aefefe0c533ae79ea3517c217ab7c262eb54e992
AntonBeletsky <antonbeletskyeu@gmail.com>
2018-03-14 10:25:17 +0200

    base structure

---
+++ app/startup/startup.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton Beletsky
+ * Date: 2/12/2018
+ * Time: 2:42 PM
+ */
+
+namespace BelMVC;
+
+// loading app
+class startup
+{
+    public function __construct()
+    {
+
+    }
+
+    public function configuration()
+    {
+
+    }
+
+    public function ConfigureServices($services = false)
+    {
+
+    }
+
+    public function Configure($app = false, $env = false)
+{
+//            if (env.IsDevelopment())
+//            {
+//            app.UseBrowserLink();
+//            app.UseDeveloperExceptionPage();
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Home/Error");
+//            }
+//            app.UseStaticFiles();
+//            app.UseMvc(routes =>
+//                        {
+//                            routes.MapRoute(
+//                                name: "default",
+//                                template: "{controller=Home}/{action=Index}/{id?}");
+//                        });
+}
+}
